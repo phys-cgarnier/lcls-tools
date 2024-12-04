26dcc8b2a840bc81d452d0817d7cf64d7e53731d
chris garnier <cgarnier@slac.stanford.edu>
2024-12-04 10:38:58 -0800

    adding a script for comparison  of  metadata fields from csv to yaml

---
+++ lcls_tools/common/devices/yaml/compare_csv_to_yaml.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import yaml
+import os
+import argparse
+import pprint
+
+
+base_path = 'lcls-tools/lcls_tools/common/devices/yaml/'
+def compare_csv_to_yaml(device,field,area):
+    fname = os.path.join(base_path,f'{area}.yaml')
+    if os.path.isfile(fname):
+        with open(fname, "r") as f:
+            data = yaml.safe_load(f)
+    pprint.pprint(data)
+
+def parse_df(df, *args):
+    a = list(args)
+    #print(a)
+    filtered_df = df[a]
+    return filtered_df
+
+    
+
+def main():
+
+    df = pd.read_csv('lcls-tools/lcls_tools/common/devices/yaml/lcls_elements.csv')
+
+    parser = argparse.ArgumentParser(description="Script for checking device information matches between csv and yaml")
+    parser.add_argument('--area', '-a', default= None, help= 'Accelerator area that you want to run the comparison for, if flag is not passed all areas will be compared')
+    parser.add_argument('--device_type', '-d', choices=['magnet','lblm','screen','wire'], default= 'magnet')
+    parser.add_argument('--field', '-f', default= None, help= 'Metadata that you want compared')
+
+    args = parser.parse_args()
+    area = args.area
+    if args.device_type == 'magnet':
+        device_types = ["SOLE", "QUAD", "XCOR", "YCOR", "BEND"]
+    elif args.device_type == 'lblm':
+        device_types = ["PROF"]
+    elif args.device_type == 'screen':
+        device_types = ["WIRE"]
+    elif args.device_type == 'wire':
+        device_types = ["LBLM"]
+
+    metadata_field = args.field
+
+
+    if area is None:
+        #print('Checking df for unique areas and creating a list to loop over')
+        areas_col = parse_df(df,'Keyword')
+        areas = set(areas_col['Keyword'].to_list())
+        print(areas)
+
+    else:
+        compare_csv_to_yaml('','',area)
+
+
+if __name__ == "__main__":
+    main()
+
+
+#df = pd.read_csv('lcls-tools/lcls_tools/common/devices/yaml/lcls_elements.csv')
+
+#print(df["Effective Length (m)"].head(10))
